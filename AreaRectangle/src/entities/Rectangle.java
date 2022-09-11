cd4d0bf5fd6967b8d360ec18c888c2b439646935
Bruno Hoinacki <108706662+BrunoHoinacki@users.noreply.github.com>
2022-09-11 03:09:43 -0300

    Commit AreaRectangle

---
+++ AreaRectangle/src/entities/Rectangle.java
@@ -0,0 +1,24 @@
+package entities;
+
+public class Rectangle {
+	public double Width;
+	public double Height;
+	
+	public double Area() {
+		return Width * Height;
+	}
+	
+	public double Perimeter() {
+		return 2 * (Width + Height);
+	}
+
+	public double Diagonal() {	
+		return Math.sqrt(Width * Width + Height * Height);
+	}
+	
+	public String toString() {
+		return String.format("AREA = %.2f%nPERIMETER = %.2f%nDIAGONAL = %.2f%n", Area(), Perimeter(), Diagonal());
+				
+		
+	}
+}
